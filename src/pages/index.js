1af1d6342f5464ca46da8f020708031b24a7fcb5
Felix Peters <felix@felixpeters.me>
2021-03-17 17:44:29 +0100

    align heading in hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <Layout>
       <SEO title="Home" />
       <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
-        <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+        <div class="px-4 lg:w-7/12 sm:px-8 xl:pr-16">
           <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
             <span class="block xl:inline">Investieren mit</span>
             <br />
@@ -39,7 +39,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+        <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-5/12 lg:h-full">
           <img
             class="absolute inset-0 w-full h-full object-cover"
             src="https://de.scalable.capital/images/3odztfgndkxn/4scXNPPCNiWOWuqA0qYi6o/8ef16afaca737641e6924dcd92b336a3/scalable_tilt_phones_DE.png"
