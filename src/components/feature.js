738eee9dcdb79eda840745f546a19520bdbba23e
Felix Peters <felix@felixpeters.me>
2021-03-18 08:57:34 +0100

    add feature section to index page

---
+++ src/components/feature.js
@@ -0,0 +1,227 @@
+import React from "react"
+
+// Vorteile:
+// - Individuelle Anlagestrategien
+// - Prognose von Rendite und Risiko
+// - Laufende Überwachung und schnelle Reaktion auf Marktbewegungen
+// - Auswahl aus Großzahl von weltweiten ETFs
+// - Keine versteckten Kosten
+// - Immer verfügbar und mobile Benachrichtigungen
+
+export default function Feature() {
+  return (
+    <div class="relative bg-white py-16 sm:py-24 lg:py-32">
+      <div class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+        <h2 class="text-base font-semibold tracking-wider text-emerald-600 uppercase">
+          Intelligent investieren
+        </h2>
+        <p class="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+          Vermögensverwaltung neu gedacht
+        </p>
+        <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+          Legen Sie Ihr Geld sorgenfrei an. Basierend auf Ihrer Risikoneigung
+          stellen unsere Algorithmen Ihr Portfolio mit global diversifizierten
+          ETFs zusammen.
+        </p>
+        <div class="mt-12">
+          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-emerald-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Individuelle Anlagestrategien
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    Mit Hilfe unserer KI wird Ihr Portfolio wird maßgeschneidert
+                    auf Ihre Ziele und Vorlieben.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-emerald-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Rendite- und Risikoprognose
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    Unsere KI simuliert eine Großzahl von möglichen Szenarien
+                    für eine genaue Risikosteuerung.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-emerald-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Laufende Überwachung
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    Der Einsatz von KI ermöglicht die blitzschnelle Reaktion auf
+                    unvorgesehene Ereignisse, damit Sie stets geschützt sind.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-emerald-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Laufend erweiterte ETF-Auswahl
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    Ihr Portfolio wird aus über 2.000 globalen ETFs für alle
+                    Anlagenklassen zusammengestellt.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-emerald-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Keine versteckten Kosten
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    Eine Gebührenstruktur ohne Kleingedrucktes: Die Verwaltung
+                    Ihres Portfolios inklusive persönlichem Service für nur
+                    0,90% p.a.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-emerald-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Immer und überall verfügbar
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    Per App und Web haben Sie jederzeit Zugriff auf Ihr Depot.
+                    Sie erhalten individuelle Benachrichtigungen zu Ihrem
+                    Portfolio.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/hero.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+export default function Hero() {
+  return (
+    <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
+      <div class="px-4 lg:w-7/12 sm:px-8 xl:pr-16">
+        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+          <span class="block xl:inline">Investieren mit</span>
+          <br />
+          <span class="block text-emerald-600 xl:inline">
+            Künstlicher Intelligenz
+          </span>
+        </h1>
+        <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+          Unsere intelligenten Algorithmen erstellen Ihr persönliches
+          ETF-Portfolio, basierend auf Ihren Wünschen und Zielen. Nach der
+          Erstellung wird Ihr Portfolio laufend und kosteneffizient überwacht.
+        </p>
+        <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
+          <div class="rounded-md shadow">
+            <a
+              href="#"
+              class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700 md:py-4 md:text-lg md:px-10"
+            >
+              Depot eröffnen
+            </a>
+          </div>
+          <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+            <a
+              href="#"
+              class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-emerald-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+            >
+              Musterportfolio erstellen
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-5/12 lg:h-full">
+        <img
+          class="absolute inset-0 w-full h-full object-cover"
+          src="https://de.scalable.capital/images/3odztfgndkxn/4scXNPPCNiWOWuqA0qYi6o/8ef16afaca737641e6924dcd92b336a3/scalable_tilt_phones_DE.png"
+          alt=""
+        />
+      </div>
+    </div>
+  )
+}
