d4dcfbf86a381d528588773bff352b1435269c94
Felix Peters <felix@felixpeters.me>
2021-06-29 09:29:02 +0200

    fix bug related to prognosis display

--- src/components/portfolio/performance.js
+++ src/components/portfolio/performance.js
@@ -1,6 +1,10 @@
 import React from "react"
 import { useMixpanel } from "gatsby-plugin-mixpanel"
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
 function formatCurrency(num) {
   return new Intl.NumberFormat("de-DE", {
     style: "currency",
@@ -22,6 +26,11 @@ export default function PortfolioPerformance({
   const bestReturn = portfolio.return_yearly + portfolio.risk_yearly
   const expectedReturn = portfolio.return_yearly
   const worstReturn = portfolio.return_yearly - portfolio.risk_yearly
+  const cases = [
+    { name: "Bester Fall", value: bestReturn },
+    { name: "Erwarteter Fall", value: expectedReturn },
+    { name: "Ungünstigster Fall", value: worstReturn },
+  ]
 
   return (
     <div class="max-w-7xl mx-auto my-8 px-4 sm:px-6 lg:px-8">
@@ -58,94 +67,57 @@ export default function PortfolioPerformance({
         </div>
       </div>
       <dl class="mt-4 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
-        <div class="px-4 py-5 sm:p-6">
-          <dt class="text-base font-normal text-gray-900">Bester Fall</dt>
-          <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
-            <div class="flex items-baseline text-2xl font-semibold text-emerald-600">
-              {formatCurrency(
-                strategy.investmentAmount *
-                  Math.pow(1 + bestReturn, investmentHorizon)
+        {cases.map(prognosis => (
+          <div class="px-4 py-5 sm:p-6">
+            <dt class="text-base font-normal text-gray-900">
+              {prognosis.name}
+            </dt>
+            <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
+              <div class="flex items-baseline text-2xl font-semibold text-emerald-600">
+                {formatCurrency(
+                  strategy.investmentAmount *
+                    Math.pow(1 + prognosis.value, investmentHorizon)
+                )}
+              </div>
+              {prognosis.value >= 0 && (
+                <div class="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0">
+                  <svg
+                    class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <span class="sr-only">Increased by</span>
+                  {formatPercentage(prognosis.value)}
+                </div>
               )}
-            </div>
-
-            <div class="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0">
-              <svg
-                class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span class="sr-only">Increased by</span>
-              {formatPercentage(bestReturn)}
-            </div>
-          </dd>
-        </div>
-
-        <div class="px-4 py-5 sm:p-6">
-          <dt class="text-base font-normal text-gray-900">Erwarteter Fall</dt>
-          <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
-            <div class="flex items-baseline text-2xl font-semibold text-emerald-600">
-              {formatCurrency(
-                strategy.investmentAmount *
-                  Math.pow(1 + expectedReturn, investmentHorizon)
+              {prognosis.value < 0 && (
+                <div class="bg-red-100 text-red-800 inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0">
+                  <svg
+                    class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <span class="sr-only">Decreased by</span>
+                  {formatPercentage(prognosis.value)}
+                </div>
               )}
-            </div>
-
-            <div class="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0">
-              <svg
-                class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span class="sr-only">Increased by</span>
-              {formatPercentage(expectedReturn)}
-            </div>
-          </dd>
-        </div>
-
-        <div class="px-4 py-5 sm:p-6">
-          <dt class="text-base font-normal text-gray-900">
-            Ungünstigster Fall
-          </dt>
-          <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
-            <div class="flex items-baseline text-2xl font-semibold text-emerald-600">
-              {formatCurrency(
-                strategy.investmentAmount *
-                  Math.pow(1 + worstReturn, investmentHorizon)
-              )}
-            </div>
-
-            <div class="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-red-100 text-red-800 md:mt-2 lg:mt-0">
-              <svg
-                class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span class="sr-only">Decreased by</span>
-              {formatPercentage(worstReturn)}
-            </div>
-          </dd>
-        </div>
+            </dd>
+          </div>
+        ))}
       </dl>
     </div>
   )
